2102ac376e762516020d78c53485439c16046417
pabloantonio <emailpabloroman@gmail.com>
2023-10-03 13:49:24 -0400

    added current weather and made a search box clickable

--- asset/script.js
+++ asset/script.js
@@ -1,11 +1,16 @@
-function getInfo(){
-    const inputName= document.getElementById("cityInput");
-    const nameOfCity= document.getElementById("nameOfCity")
-    nameOfCity.innerHTML ="--" +inputName.value+"--"
-}
-
 var apiKey = "f793886684d26495c42d8467c2445472"
-fetch(`https://api.openweathermap.org/geo/1.0/direct?q=Orlando,&limit=5&appid=${apiKey}`)
+   const submit= document.getElementById("submit")
+   const current= document.getElementById("current")
+function getInfo(){
+    const locationInput= document.getElementById("locationInput")
+    // const inputName= document.getElementById("cityInput");
+    // const nameOfCity= document.getElementById("nameOfCity")
+    // nameOfCity.innerHTML ="--" +inputName.value+"--"
+    getGeo(locationInput.value)
+}
+function getGeo(city){
+
+fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${apiKey}`)
 .then((response) => response.json()).then((data) => {
 
 
@@ -13,6 +18,25 @@ longitude= data[0].lon;
 latitude= data[0].lat;
 
 console.log(latitude);
+getFiveDay(latitude,longitude)
+getCurrent(latitude,longitude)
+})}
+function getCurrent(latitude,longitude){
+    fetch (`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=imperial`)
+    .then((response) => response.json()).then((data) => { 
+        console.log(data)
+        var temp= document.createElement("p")
+        var wind= document.createElement("p")
+        var humidity= document.createElement("p")
+        var img= document.createElement("img")
+        temp.textContent=data.main.temp
+        wind.textContent=data.wind.speed
+        humidity.textContent=data.main.humidity
+        img.setAttribute("src","https://openweathermap.org/img/wn/"+data.weather[0].icon+"@2x.png")
+        current.append(img,temp,wind,humidity)
+    })
+}
+function getFiveDay(latitude,longitude){
 
 
 fetch (`https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=imperial`)
@@ -27,6 +51,7 @@ for (var i = 0; i < data.list.length; i+=8) {
     currentTemp=`${data.list[i].main.temp}`
     humidity= data.list[i].main.humidity
     wind= data.list[i].wind.speed
+    var dt = data.list[i].dt
     console.log(currentTemp);
     var tempEl = document.getElementById("day"+counter+"Min")
     tempEl.textContent = currentTemp
@@ -36,16 +61,11 @@ for (var i = 0; i < data.list.length; i+=8) {
     windEl.textContent = wind
     var imgEl = document.getElementById("img"+counter)
     imgEl.src = url
+    var day = document.getElementById("day"+counter)
+    day.textContent = dayjs.unix(dt).format("dddd")
     counter++
 }
 
-
-
-
-
-
-
-
-
 });
-});
+};
+submit.addEventListener("click",getInfo)
